b97d8820815138d26ba6f0b2e6b94432cbf0944c
Adam Hosker <adam@hosker.info>
2025-09-18 17:07:28 +0000

    Add Livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-//
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+// Alpine Plugin Example
+//import Clipboard from '@ryangjchandler/alpine-clipboard'
+//Alpine.plugin(Clipboard)
+
+Livewire.start()
