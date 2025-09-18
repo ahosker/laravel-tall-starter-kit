948d7c303a3ffd59b6d72d0517dae91a0ac47d95
Adam Hosker <adam@hosker.info>
2025-09-18 21:51:00 +0000

    Add fluxui.dev

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,14 +7,21 @@
 
     <title>{{ $title ?? 'Page Title' }}</title>
 
+    <!-- LiveWire Styles -->
     @livewireStyles
+    <!-- VITE -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <!-- FLux Styles -->
+    @fluxAppearance
+    <!-- Flux Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 </head>
 
 <body>
     {{ $slot }}
 
+    @fluxScripts
     @livewireScriptConfig
 </body>
 
